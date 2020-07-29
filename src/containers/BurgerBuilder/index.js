23de45065cfa846c13742385ad6d51fe803c6953
Iván Bazaldúa <icesarbr@gmail.com>
2020-07-29 00:52:22 -0500

    My first commit, Burger and BurgerIngredients components finished

---
+++ src/containers/BurgerBuilder/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger'
+
+class BurgerBuilder extends Component {
+    render(){
+        return (
+            <Aux>
+                <Burger />
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
