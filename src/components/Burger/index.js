37f445b6f491afa9f9fe922a3341584b6495ae3a
Iván Bazaldúa <icesarbr@gmail.com>
2020-07-29 22:32:33 -0500

    add ingredient functionality OK

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Burger.module.css';
-import BurgerIngredient from './BurgerIngredient'
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient";
 
-const burger = () => {
-    return (
-        <div  className={classes.Burger} >
-            <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="bread-bottom" />
-        </div>
-    );
-}
+const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((ingredKey) => {
+      return [...Array(props.ingredients[ingredKey])].map((_, i) => {
+        return <BurgerIngredient key={ingredKey + i} type={ingredKey} />;
+      });
+    })
+    .reduce((acumulador, valorActual) => {
+      return acumulador.concat(valorActual);
+    }, []);
+
+  if(transformedIngredients.length === 0){
+      transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
 
 export default burger;
